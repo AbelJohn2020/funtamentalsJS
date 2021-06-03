5eedc0b7cb5ad2c0566fbfbe93729137c8107df1
Abel AR <abel_john_14@hotmail.com>
2021-06-02 23:28:51 -0500

    important class

--- fundamentals-js/classInJsIMPORTANT.js
+++ fundamentals-js/classInJsIMPORTANT.js
@@ -4,6 +4,18 @@
 
 // and to save in this object that we are building in the RAM, we can reference this object in this function like a "THIS"
 // y para guardarlo dentro de este objeto que ese esta contruyendo dentro de la memoria, podemos hacer referencia a ese objeto dentro de esta funcion como 'this'.
+
+
+// __proto__ apunta al protoripo que tiene la funcion que en este caso es el objeto, son todos los metodos que tienen todos los objetos y todos los atributos que tienen todos los objetos
+
+// la herencia como tal no existe en JS no existen clases sino prototipos, existe la herencia prototipada. Y funciona cuando a un objeto de desarrollador como en este caso person se le decia que ejecute el metodo "height" lo qeu iba a hacer era dijarse en el en su prototipo __proto__ si encuentra ese metodo sino lo encuentra ira a buscarlo al prototipo de ese prototipo y asi sucesivameente se pasaran mensajes entre los prototipos hasta ver si alguno lo resuelve. Si ninguno lo llega a resolver llegara hasta object y object tratara de ejecutar ese metodo y sino puede ejecutarlo, se lanzara un error en JS.
+
+function ComesFrom(sonPrototype, faherPrototype) {
+    const fn = function() {};
+    fn.prototype = faherPrototype.prototype;
+    sonPrototype.prototype = new fn;
+    sonPrototype.prototype.constructor = sonPrototype;
+}
 function Person(name, lastname, heightt) {
     this.name = name; // name after this is the atribute of the created object and name after equal sign is the value of the paramether.
     this.lastname = lastname
@@ -11,6 +23,12 @@ function Person(name, lastname, heightt) {
     this.heightt = heightt
 }
 
+function Developer(name, lastname) {
+    this.name = name;
+    this.lastname = lastname;
+}
+
+// PERSON
 Person.prototype.sayHello = function() {
     console.log(`Hello my name is: ${this.name} ${this.lastname}`)
 }
@@ -23,9 +41,19 @@ Person.prototype.height = function () {
     }
 }
 
+// DEVELOPER
+ComesFrom(Developer, Person)
+Developer.prototype.sayHello = function () {
+    console.log(`My name is ${this.name} ${this.lastname} and I'm a developer`)
+}
+
 // new before the prototype is the responsible to create a new object, and that object is going to be assigned like a prototype, the protorype that we assigned after.
 
+
 const person = new Person('Abel', 'Arbildo', 1.62);
+const developer = new Developer('Abel', 'Arbildo');
 
 person.sayHello();
 person.height();
+
+developer.sayHello();
