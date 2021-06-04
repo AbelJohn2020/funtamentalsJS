d5237a07d1fdffe656abff65aa25fe6a8def9c4e
Abel AR <abel_john_14@hotmail.com>
2021-06-04 00:04:54 -0500

    Add callbacks and promises classes

---
+++ fundamentals-js/callback.js
@@ -0,0 +1,72 @@
+const base_uri = 'https://swapi.dev/api/';
+const people_url = 'people/:id';
+
+const option = { crossDomain: true };
+
+function getAperson (id) {
+    return new Promise((resolve, reject) => {
+        const url = `${base_uri}${people_url.replace(':id', id)}/`;
+        
+        //this is the way to call jQuery
+        $
+            .get(url, option, function(data) {
+                resolve(data)
+            })
+            .fail(() => reject(id))
+    })
+
+}
+
+function onError(id) {
+    console.error(`An error occurred while getting the character ${id}`);
+}
+
+getAperson(1)
+    .then(function(data) {
+        console.log(data)
+        return getAperson(2)
+    })
+    .then(function(data) {
+        console.log(data)
+        return getAperson(3)
+    })
+    .then(function(data) {
+        console.log(data)
+        return getAperson(4)
+    })
+    .then(function(data) {
+        console.log(data)
+        return getAperson(5)
+    })
+    .then(function(data) {
+        console.log(data)
+        return getAperson(6)
+    })
+    .then(function(data) {
+        console.log(data)
+        return getAperson(7)
+    })
+    .then(function(data) {
+        console.log(data)
+    })
+    .catch(onError)
+
+// getAperson(1, function(data) {
+//     console.log(data);
+//     getAperson(2, function(data) {
+//         console.log(data);
+//         getAperson(3, function(){
+//             getAperson(4, function(data) {
+//             console.log(data);
+//                 getAperson(5, function(data) {
+//                 console.log(data);
+//                     getAperson(6, function(data) {
+//                     console.log(data);
+//                         getAperson(7, function(data) {
+//                             console.log(data);})
+//                     })
+//                 })
+//             })
+//         })
+//     })
+// })
--- fundamentals-js/callbacks.js
+++
@@ -1,7 +0,0 @@
-import { base_uri, people_url } from './confing.js'
-
-const lukeUrl = `${base_uri}${people_url.replace(':id', 1)}`;
-const option = { crossDomain: true };
-
-//this is the way to call jQuery
-$.get(lukeUrl, option)
