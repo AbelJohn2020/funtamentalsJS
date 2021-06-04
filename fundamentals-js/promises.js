4ffc2600aae61c38ccbd140054cdf313665acb70
Abel AR <abel_john_14@hotmail.com>
2021-06-04 00:14:56 -0500

    Add promises with request

--- fundamentals-js/promises.js
+++ fundamentals-js/promises.js
@@ -1,11 +1,11 @@
-const base_uri = 'https://swapi.dev/api/';
-const people_url = 'people/:id';
+const BASE_URI = 'https://swapi.dev/api/';
+const PEOPLE_URL = 'people/:id';
 
-const option = { crossDomain: true };
+const secondOption = { crossDomain: true };
 
 function getAperson (id) {
     return new Promise((resolve, reject) => {
-        const url = `${base_uri}${people_url.replace(':id', id)}/`;
+        const url = `${BASE_URI}${PEOPLE_URL.replace(':id', id)}/`;
         
         //this is the way to call jQuery
         $
@@ -21,32 +21,10 @@ function onError(id) {
     console.error(`An error occurred while getting the character ${id}`);
 }
 
-getAperson(1)
-    .then(function(data) {
-        console.log(data)
-        return getAperson(2)
-    })
-    .then(function(data) {
-        console.log(data)
-        return getAperson(3)
-    })
-    .then(function(data) {
-        console.log(data)
-        return getAperson(4)
-    })
-    .then(function(data) {
-        console.log(data)
-        return getAperson(5)
-    })
-    .then(function(data) {
-        console.log(data)
-        return getAperson(6)
-    })
-    .then(function(data) {
-        console.log(data)
-        return getAperson(7)
-    })
-    .then(function(data) {
-        console.log(data)
-    })
+const ids = [1, 2, 3, 4, 5, 6, 7];
+
+const promises = ids.map(id => getAperson(id))
+Promise
+    .all(promises)
+    .then(data => console.log(data))
     .catch(onError)
